de7901f200c4f599e5a476667da3452472f173f3
lundbergg <dennis.p.lundberg@gmail.com>
2015-10-14 13:16:27 +0200

    Stop tracking unnecessary files and directories

--- emacs.d/elpa/flycheck-ats2-20150628.1845/flycheck-ats2-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; flycheck-ats2-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "flycheck-ats2" "flycheck-ats2.el" (21991 7983
-;;;;;;  118938 475000))
-;;; Generated autoloads from flycheck-ats2.el
-
-(autoload 'flycheck-ats2-setup "flycheck-ats2" "\
-Set up Flycheck ATS2.
-
-Add `ats2' to `flycheck-checkers'.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flycheck-ats2-autoloads.el ends here
